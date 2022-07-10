36407567e95b8dd12931433fbd63e9fecd0ddc2a
McDonald Namba <nambamcdonald@gmail.com>
2022-07-10 15:13:13 +0200

    Setup graphql with apollo-client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,22 @@
+import { useQuery } from '@apollo/client'
 import type { NextPage } from 'next'
+import RecommendedProfiles from '../graphql/recommendedProfiles.graphql'
 
 const Home: NextPage = () => {
+
+  const { data, error, loading } = useQuery(RecommendedProfiles)
+
+  if(error){ return (<div>{error.message}</div>) }
+  if(loading){ return (<div>Loading....</div>)}
+  if(!data){ return (<div>Nothing to show</div>)}
+
   return (
     <div className='text-red-700'>
-      I can do whatever to make you smile
+      {
+        data.recommendedProfiles.map((profile) => (
+          <div key={profile.id}>{profile.name}</div>
+        ))
+      }
     </div>
   )
 }
